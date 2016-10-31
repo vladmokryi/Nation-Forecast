0b317f12634de37bd749a2312241203254bcdb5a
Vlad <vladislav.mokriy@isells.eu>
2016-10-31 10:30:30 +0200

    add process env

--- server/config.js
+++ server/config.js
@@ -3,16 +3,19 @@ const config = {
   port: process.env.PORT || 8000,
   providers: {
     openweathermap: {
-      apiUrl: "http://api.openweathermap.org/data/2.5/forecast/daily",
-      apiKey: "b207ffe7c6ab004ce89ad05adb085b9b"
+      apiUrl: process.env.OPENWEATHERMAP_APIURL,
+      apiKey: process.env.OPENWEATHERMAP_APIKEY
     },
     apixu: {
-      apiUrl: "http://api.apixu.com/v1/forecast.json",
-      apiKey: "a549977375344f3fb49201620161810"
+      apiUrl: process.env.APIXU_APIURL,
+      apiKey: process.env.APIXU_APIKEY
     }
   },
-  actualCachePeriod: 43200000,
-  forecastPeriod: 7
+  google: {
+    apikey: process.env.GOOGLE_APIKEY
+  },
+  actualCachePeriod: process.env.ACTUAL_CACHE_PERIOD || 43200000,
+  forecastPeriod: process.env.FORECAST_PERIOD || 7
 };
 
 export default config;
